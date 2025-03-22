991a0e3ea996607bb82ca5e72977d2891c80fa1b
Nicodry20 <48178309@est.ort.edu.ar>
2025-03-22 13:34:53 -0300

    empresaLista

--- empresa.py
+++ empresa.py
@@ -1,6 +1,7 @@
-
+import math
 nombre = input("ingrese nombre \n")
-viajeros = int( input())
-acompañantes = int( input())
-precios = int( input())
-
+viajeros = int(input("ingrese la cantidad de viajeros \n"))
+acompañantes = math.ceil (viajeros / 30) if viajeros > 0 else 0
+precio = 500 * (viajeros + acompañantes)
+print(f"{nombre}, habrá {acompañantes} acompañante(s) en el viaje.")
+print(f"El costo total del viaje es: ${precio}")
